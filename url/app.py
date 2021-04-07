f29b6563e829e8e4d78bbee50018f0c6886cdad8
arichilles <arifnd77@gmail.com>
2021-04-07 08:00:26 +0800

    Bagian Ke-6 Menetukan rute url

---
+++ url/app.py
@@ -0,0 +1,17 @@
+from flask import *
+
+application = Flask(__name__)
+@application.route('/')
+def index():
+    return render_template('home.html')
+
+@application.route('/product')
+def product():
+    return render_template('/product.html')
+
+@application.route('/contact')
+def contact():
+    return render_template('/contact.html')
+
+if __name__ == "__main__":
+    application.run(debug=True)
