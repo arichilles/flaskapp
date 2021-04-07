32cb568397ca7f25cd226f2f910e67b3dba65ae7
arichilles <arifnd77@gmail.com>
2021-04-07 08:25:01 +0800

    Bagian Ke-7 Melewatkan Parameter ke dalam url

--- parameter/app.py
+++ parameter/app.py
@@ -10,5 +10,10 @@ def hello(name):
 def nomor(no):
     return '<h2>Ini merupakan urutan ke-%d</h2>' % no
 
+# mendefinisikan paramater bertipe float 
+@app.route('/number/<float:nom>')
+def number(nom):
+    return '<h2>Ini merupakan bilangan riil dengan tipe floa urut ke-%.1f </h2>' % nom
+
 if __name__ == "__main__":
     app.run(debug=True)
